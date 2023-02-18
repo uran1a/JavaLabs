4f4d900e94ce69428b8233ee3d4a8415719e7bcf
uran1a <tydom24@gmail.com>
2023-02-18 18:25:49 +0300

    new commit

--- src/main/java/labs/Main.java
+++ src/main/java/labs/Main.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Task1 task = new Task1();
+
         System.out.print("Введите высоту: ");
-        System.out.print(task.calculateFigure1(new Scanner(System.in).nextInt()));
+        int height = new Scanner(System.in).nextInt();
+        System.out.print(task.calculateFigure1(height));
         System.out.println();
         System.out.println();
         System.out.print("Введите высоту: ");
-        System.out.print(task.calculateFigure2(new Scanner(System.in).nextInt()));
+        System.out.print(task.calculateFigure2(height));
     }
 }
